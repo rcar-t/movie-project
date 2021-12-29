c5bfe76ac16d071202a1407d4a0917aeea37abd5
Rhonda Toh <66073079+rcar-t@users.noreply.github.com>
2021-12-29 18:01:38 +0800

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -8,8 +8,6 @@ export const environment = {
   MOVIE_DB_URL: "https://api.themoviedb.org/3",
   MOVIE_API_KEY: "6bd573c59cf67b602cacc8c66d003003",
   MOVIE_READ_ACCESS_TOKEN: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YmQ1NzNjNTljZjY3YjYwMmNhY2M4YzY2ZDAwMzAwMyIsInN1YiI6IjYwZDJlZjkyNjdiNjEzMDAyYzY1ZGIyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XGBQiSKGOW2fO7lMzkCrTceu4yumnSQ_Pno38rxkuSY",
-  aws_secret_key: "IDyf/wiixTRlB0vT/ovR1YjiTraw0WSTZo3NxJ6v",
-  aws_access_key: "AKIATHXQVPDEO3WXF74S"
 };
 
 /*
